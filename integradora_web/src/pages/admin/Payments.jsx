2da5374156fae22a6889e998705a3181dbebbf24
AlanYahirAguilar <alanyr107@gmail.com>
2025-04-14 18:25:28 -0600

    cambios en payment

--- integradora_web/src/pages/admin/Payments.jsx
+++ integradora_web/src/pages/admin/Payments.jsx
@@ -11,41 +11,43 @@ import PaymentList from "../../components/admin/PaymentList";
 import Sidebar from "../../components/admin/Sidebar";
 
 import defaultProfile from "../../assets/img/unknow.jpeg";
+import { useUserContext } from "../../contexts/UserProvider";
 
 const Payments = () => {
-    const [selectedFilter, setSelectedFilter] = useState("FINISHED");
+  const { user } = useUserContext();
+  const [selectedFilter, setSelectedFilter] = useState("FINISHED");
 
-    useEffect(() => {
-        const existingPayments = JSON.parse(localStorage.getItem("payments") || "[]")
+  useEffect(() => {
+    const existingPayments = JSON.parse(localStorage.getItem("payments") || "[]")
 
-        if (existingPayments.length === 0) {
-            const mockPayment = {
-              id: 3,
-              studentName: "Erick Ramírez",
-              courseName: "React Básico",
-              cost: 199.99,
-              status: "Pendiente",
-              instructor: "Vanessa Juárez",
-              email: "erick@example.com",
-              startDate: "2024-05-10",
-              endDate: "2024-06-10",
-              profileUser: defaultProfile,
-              voucher: "https://pub-c3312bde490a4f4e8e5d3ce1606e7208.r2.dev/09b44eac-ef81-4750-b685-cb6cbb238e4e-Manual virtualizacion Laura.pdf"
-            }
+    if (existingPayments.length === 0) {
+      const mockPayment = {
+        id: 3,
+        studentName: "Erick Ramírez",
+        courseName: "React Básico",
+        cost: 199.99,
+        status: "Pendiente",
+        instructor: "Vanessa Juárez",
+        email: "erick@example.com",
+        startDate: "2024-05-10",
+        endDate: "2024-06-10",
+        profileUser: defaultProfile,
+        voucher: "https://pub-c3312bde490a4f4e8e5d3ce1606e7208.r2.dev/09b44eac-ef81-4750-b685-cb6cbb238e4e-Manual virtualizacion Laura.pdf"
+      }
 
-            localStorage.setItem("payments", JSON.stringify([mockPayment]))
-            window.dispatchEvent(new Event("storage"))
-        }
-    }, [])
+      localStorage.setItem("payments", JSON.stringify([mockPayment]))
+      window.dispatchEvent(new Event("storage"))
+    }
+  }, [])
 
-    return (
-        <>
-        <Sidebar />
-      <Header userName={"Vanessa Juárez"} />
+  return (
+    <>
+      <Sidebar />
+      <Header userName={user?.name} />
       <section className={styles.content}>
         <ControlPanel
-        showSearch={true}
-        showToggle={true}
+          showSearch={true}
+          showToggle={true}
           selectedFilter={selectedFilter} /* Indica que opción del filtro está activa */
           setSelectedFilter={setSelectedFilter}
           toggleOptions={[
@@ -58,11 +60,11 @@ const Payments = () => {
           ]} /* Define las etiquetas amigables para mostrar */
         />
 
-        <PaymentList selectedFilter={selectedFilter}/>
+        <PaymentList selectedFilter={selectedFilter} />
       </section>
       <Footer />
-        </>
-    )
+    </>
+  )
 
 
 }
